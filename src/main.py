7f555f9144cdf8b5af714bda07c4ad33e2e36c09
mirkocuchan <mirko.cuchan01@gmail.com>
2026-01-17 04:43:17 +0000

    fixed

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ dir_path_static = "./static"
 dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-default_basepath = "/static-page/"
+default_basepath = "/"
 
 def main():
     basepath = default_basepath
@@ -61,8 +61,8 @@ def generate_page(from_path, template_path, dest_path, basepath):
     final_html = template.replace("{{ Title }}", title)
     final_html = final_html.replace("{{ Content }}", html_content)
 
-    final_html = final_html.replace('href="/"', f'"href={basepath}"')
-    final_html = final_html.replace('"src="/', f'"src={basepath}"')
+    final_html = final_html.replace('href="/', f'href="{basepath}')
+    final_html = final_html.replace("src=/", f"src={basepath}")
 
 
     dest_dir = os.path.dirname(dest_path)
